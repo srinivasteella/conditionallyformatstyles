47e174e23667fec537991f35f1b1702143f64a4c
srinivasteella <srinivas.teella@gmail.com>
2023-05-05 11:31:22 +1000

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderV1Module } from './headerv1/headerv1.module';
 import { DropdowncompComponent } from './dropdowncomp/dropdowncomp.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DropdowncompComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HeaderV1Module,
-    NgSelectModule
-  ],
+  declarations: [AppComponent, DropdowncompComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, NgSelectModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
